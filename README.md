1565f0ab4cb54957572dc2b3ed2213986198b969
alexo <alexollemail@gmail.com>
2025-04-08 21:20:36 +0200

    Minuto 58:56, controladores pero faltan las vistas y poner en castellano los elementos

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
+https://www.youtube.com/watch?v=VrQRa-afCAk&t=685s    :  min 58:56
+TODO URGENTE: 
+- ver los permisos con inertia(https://medium.com/@laravelprotips/implementing-roles-permissions-in-laravel-using-inertia-a-guide-with-breeze-jetstream-a9fe480977de)
+- Crear los ducumentos de las vistas 
+- Hacer los elementos en español con localization
 
-TODO URGENTE: ver los permisos con inertia(https://medium.com/@laravelprotips/implementing-roles-permissions-in-laravel-using-inertia-a-guide-with-breeze-jetstream-a9fe480977de)
 
-
-
-https://www.youtube.com/watch?v=VrQRa-afCAk&t=685s    :  min 24:46
 Apunte 1: PAra el desarrollo de este proyecto se está usando colorful comments(vscode extension), por eso aparecen símbolos extrños en los comentarios(?,*,&,~....)
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,35 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+Route::redirect('/','/dashboard');//*1.Usuario llega, es redirigido a dashboard
+
+
+Route::middleware(['auth','verified'])->group(function(){//Si el usuario ha iniciado sesión, puede acceder a las rutas
+    //*2.Usuario pasa por el middleware para poder acceder, si es digno(Mjölnir a mi) pasa a la vista Dashboard administrada por inertia(obsidian://open?vault=TFC-Vault&file=TFC%2FTheCodeholic%20example%2Finertia%2FDocumento%20principal--%20App.jsx)
+    Route::get('/dashboard', fn ()=> Inertia::render('Dashboard')
+        ->name('dashboard')//Si recibe la ruta, devuelve el renderizado del componente de inertia
+    );
+    //?Esto es básicamente barra libre, se prestan las rutas de los controladores como rutas uri
+    Route::resource('project',ProjectController::class);
+    Route::resource('task',TaskController::class);
+    Route::resource('user',UserController::class);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
