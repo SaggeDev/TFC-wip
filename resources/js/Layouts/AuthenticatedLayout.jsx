18681fe30806ebaea02660b4d8b126b57af06c9a
alexo <alexollemail@gmail.com>
2025-04-12 22:27:19 +0200

    Empezado con los proyectos

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -59,7 +59,7 @@ export default function AuthenticatedLayout({ header, children }) {
                                         <span className="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center rounded-md bg-gray-100 px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 focus:outline-none transition duration-150 ease-in-out hover:text-gray-700  dark:bg-gray-800 dark:text-gray-200 dark:hover:text-gray-400"
+                                                className="inline-flex items-center rounded-md bg-gray-100 px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 focus:outline-none transition duration-150 ease-in-out hover:text-gray-700  dark:bg-gray-700 dark:text-gray-200 dark:hover:text-gray-400"
                                             >
                                                 {user.name}
 
