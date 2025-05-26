0f4b1ab8fbc48e7d729fa5151cb1db7eeebcecb7
RaccH <alexollemail@gmail.com>
2025-05-26 10:49:05 +0200

    Ahora si, acabado

--- Arduino/Entee_Definitivo_copy_20250505225011/CONSTANTS.h
+++
@@ -1,9 +0,0 @@
-#define SECRET_SSID "doomsday" 
-#define SECRET_PASS "elblocdenotas" 
-
-#define SERVER_IP "192.168.0.132" 
-
-#define CHECK_URL "api/checkNFC" //Si falla es porque falta / al principio
-#define ARDUINOKEY "ArduinoKey" 
-
-#define ADMIN_URL "api/registerNFC" 
